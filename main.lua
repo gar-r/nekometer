48f403717ca3c40ff6534e1bf8a8b2e616d275b5
Richard Garai <garric@okki.hu>
2023-12-14 21:08:13 +0100

    addon skeleton

--- main.lua
+++ main.lua
@@ -1,1 +1,18 @@
-print("hello")
+local addonName, ns = ...
+
+---@class frame
+local frame = CreateFrame("Frame", "NekometerMain")
+
+function frame:OnEvent(event, ...)
+    self[event](self, event, ...)
+end
+
+function frame:ADDON_LOADED(_, name)
+    if name == addonName then
+        ns.version = C_AddOns.GetAddOnMetadata(name, "Version")
+        print(addonName .. " version " .. ns.version .. " loaded")
+    end
+end
+
+frame:RegisterEvent("ADDON_LOADED")
+frame:SetScript("OnEvent", frame.OnEvent)
---
+++ slash.lua
@@ -0,0 +1,8 @@
+SLASH_NEKOMETER1 = "/nm"
+SLASH_NEKOMETER2 = "/nekometer"
+
+local function SlashCommandHandler(_, _)
+    print("hello")
+end
+
+SlashCmdList["NEKOMETER"] = SlashCommandHandler
