73d12e3a95436625ab1658a24721a68c46a67bdb
Richard Garai <garric@okki.hu>
2023-12-18 22:12:20 +0100

    refactor commands

--- core/slash.lua
+++ core/slash.lua
@@ -3,15 +3,31 @@ SLASH_NEKOMETER2 = "/nekometer"
 
 local _, nekometer = ...
 
-local function SlashCommandHandler(msg, _)
-    if msg == "config" then
-        InterfaceOptionsFrame_OpenToCategory(nekometer.frames.config)
-    elseif msg == "reset" then
-        StaticPopup_Show("NEKOMETER_RESET")
-    elseif msg == "report" then
-        local m = nekometer.damage
-        nekometer.PrintReport(m:Report())
+local commands = {}
+
+function commands:config(_)
+    InterfaceOptionsFrame_OpenToCategory(nekometer.frames.config)
+end
+
+function commands:reset(_)
+    StaticPopup_Show("NEKOMETER_RESET")
+end
+
+function commands:report(msg)
+    local meter = nekometer.damage
+    if msg and nekometer[msg] and nekometer[msg].Report then
+        meter = nekometer[msg]
     end
+    nekometer.PrintReport(meter:Report())
+end
+
+local function SlashCommandHandler(msg, _)
+    local verb, args = msg:match("^(%S*)%s*(.-)$")
+    local cmd = commands.config
+    if verb and commands[verb] then
+        cmd = commands[verb]
+    end
+    cmd(commands, args)
 end
 
 SlashCmdList["NEKOMETER"] = SlashCommandHandler
