cb5ae56425af14d12e1ed1de9f487221602a3c7b
Richard Garai <garric@okki.hu>
2023-12-21 12:08:35 +0100

    add config frame controls

--- ui/config.lua
+++ ui/config.lua
@@ -1,8 +1,11 @@
 local _, nekometer = ...
 
+local config = nekometer.config
+
 ---@class frame
-local frame = CreateFrame("Frame", "NekometerConfig", UIParent)
+local frame = CreateFrame("Frame", "NekometerConfig")
 frame.name = "Nekometer"
+InterfaceOptions_AddCategory(frame)
 
 function frame:CreateSlider(min, max, step)
     local slider = CreateFrame("Slider", nil, self, "OptionsSliderTemplate")
@@ -26,24 +29,61 @@ function frame:CreateCheckbox(text)
     return checkbox
 end
 
-local dpsRefreshSlider = frame:CreateSlider(1, 30, 1)
-dpsRefreshSlider:SetPoint("TOP", frame, 0, -20)
-dpsRefreshSlider:SetValue(2)
+local title = frame:CreateFontString(nil, "ARTWORK", "GameFontNormalLarge")
+title:SetPoint("TOPLEFT", 16, -16)
+title:SetText("Nekometer Settings")
 
-local dpsSmoothingSlider = frame:CreateSlider(0.1, 0.9, 0.1)
-dpsSmoothingSlider:SetPoint("BOTTOM", dpsRefreshSlider, 0, -40)
-dpsSmoothingSlider:SetValue(0.7)
+local catGlobal = frame:CreateFontString(nil, "ARTWORK", "GameFontHighlight")
+catGlobal:SetPoint("TOPLEFT", title, "BOTTOMLEFT", 0, -16)
+catGlobal:SetText("Global options")
+
+local mergePetsCheckbox = frame:CreateCheckbox("Merge Pets with their owners")
+mergePetsCheckbox:SetPoint("TOPLEFT", catGlobal, "BOTTOMLEFT", 0, -8)
+
+local classColorsCheckbox = frame:CreateCheckbox("Use class colors for bars")
+classColorsCheckbox:SetPoint("TOPLEFT", mergePetsCheckbox, "BOTTOMLEFT", 0, -8)
+
+local catMeters = frame:CreateFontString(nil, "ARTWORK", "GameFontHighlight")
+catMeters:SetPoint("TOPLEFT", classColorsCheckbox, "BOTTOMLEFT", 0, -16)
+catMeters:SetText("Enabled meters")
+
+local damageCheckbox = frame:CreateCheckbox("Damage")
+damageCheckbox:SetPoint("TOPLEFT", catMeters, "BOTTOMLEFT", 0, -8)
+
+local dpsCurrentCheckbox = frame:CreateCheckbox("Dps (current)")
+dpsCurrentCheckbox:SetPoint("TOPLEFT", damageCheckbox, "BOTTOMLEFT", 0, -8)
+
+local dpsCombatCheckbox = frame:CreateCheckbox("Dps (combat)")
+dpsCombatCheckbox:SetPoint("TOPLEFT", dpsCurrentCheckbox, "BOTTOMLEFT", 0, -8)
+
+local healingCheckbox = frame:CreateCheckbox("Healing")
+healingCheckbox:SetPoint("TOPLEFT", dpsCombatCheckbox, "BOTTOMLEFT", 0, -8)
+
+local catAdvanced = frame:CreateFontString(nil, "ARTWORK", "GameFontHighlight")
+catAdvanced:SetPoint("TOPLEFT", healingCheckbox, "BOTTOMLEFT", 0, -16)
+catAdvanced:SetText("Advanced options")
+
+local windowSliderText = frame:CreateFontString(nil, "ARTWORK", "GameFontNormal")
+windowSliderText:SetPoint("TOPLEFT", catAdvanced, "BOTTOMLEFT", 0, -16)
+windowSliderText:SetText("Window size")
+
+local windowSlider = frame:CreateSlider(1, 10, 1)
+windowSlider:SetPoint("TOPLEFT", windowSliderText, "BOTTOMLEFT", 0, -8)
+windowSlider:SetValue(3)
+
+local smoothingSliderText = frame:CreateFontString(nil, "ARTWORK", "GameFontNormal")
+smoothingSliderText:SetPoint("TOPLEFT", windowSlider, "BOTTOMLEFT", 0, -16)
+smoothingSliderText:SetText("Smoothing factor")
+
+local smoothingSlider = frame:CreateSlider(0.1, 0.9, 0.1)
+smoothingSlider:SetPoint("TOPLEFT", smoothingSliderText, "BOTTOMLEFT", 0, -8)
+smoothingSlider:SetValue(0.7)
 
 local saveButton = frame:CreateButton("Save")
-saveButton:SetPoint("CENTER", dpsSmoothingSlider, 0, -40)
+saveButton:SetPoint("BOTTOM", frame, 70, 10)
 
-local resetButton = frame:CreateButton("Reset Settings")
-resetButton:SetPoint("CENTER", saveButton, 0, -40)
-
-local checkbox = frame:CreateCheckbox("Text Checkbox")
-checkbox:SetPoint("CENTER", resetButton, 0, -40)
-
-InterfaceOptions_AddCategory(frame)
+local resetButton = frame:CreateButton("Reset")
+resetButton:SetPoint("BOTTOM", frame, -70, 10)
 
 nekometer.frames = nekometer.frames or {}
 nekometer.frames.config = frame
