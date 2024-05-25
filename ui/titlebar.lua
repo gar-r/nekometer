3548f313ddd58aca6b1e1773a693afb7ea26f76c
Richard Garai <garric@okki.hu>
2024-05-25 20:43:27 +0200

    improve titlebar highlight

--- ui/titlebar.lua
+++ ui/titlebar.lua
@@ -34,48 +34,58 @@ frame:SetScript("OnMouseUp", function(_, button)
     end
 end)
 
-local function CreateTitleBarButton(texturePath, onClick)
+local function CreateTitleButtonTexture(button, path, drawLayer, coord)
+    local texture = button:CreateTexture(nil, drawLayer)
+    texture:SetTexture(path)
+    if coord then
+        texture:SetTexCoord(coord.left, coord.right, coord.up, coord.down)
+    end
+    return texture
+end
+
+local function CreateTitleBarButton(texturePath, onClick, texCoord)
     local button = CreateFrame("Button", nil, frame)
-    local texture = button:CreateTexture(nil, "BACKGROUND")
-    texture:SetTexture(texturePath)
-    button["texture"] = texture
     button:SetSize(14, 14)
-    button:SetNormalTexture(texture)
-    button:SetHighlightTexture("Interface\\Buttons\\ButtonHilight-Square")
-    button:GetHighlightTexture():SetBlendMode("ADD")
+    button:SetNormalTexture(CreateTitleButtonTexture(button, texturePath, "BACKGROUND", texCoord))
+    button:SetHighlightTexture(CreateTitleButtonTexture(button, texturePath, "HIGHLIGHT", texCoord))
     button:SetScript("OnClick", onClick)
     return button
 end
 
-local prevButton = CreateTitleBarButton("Interface\\CHATFRAME\\ChatFrameExpandArrow", function ()
-    mainFrame:PrevMeter()
-    titleText:SetText(mainFrame:GetCurrentMeter().title)
-end)
-prevButton["texture"]:SetTexCoord(1, 0, 0, 1)
+local prevButton = CreateTitleBarButton("Interface\\CHATFRAME\\ChatFrameExpandArrow",
+    function()
+        mainFrame:PrevMeter()
+        titleText:SetText(mainFrame:GetCurrentMeter().title)
+    end,
+    { left = 1, right = 0, up = 0, down = 1 })
 prevButton:SetPoint("LEFT", frame, "LEFT", 5, 0)
 
-local nextButton = CreateTitleBarButton("Interface\\CHATFRAME\\ChatFrameExpandArrow", function ()
-    mainFrame:NextMeter()
-    titleText:SetText(mainFrame:GetCurrentMeter().title)
-end)
+local nextButton = CreateTitleBarButton("Interface\\CHATFRAME\\ChatFrameExpandArrow",
+    function()
+        mainFrame:NextMeter()
+        titleText:SetText(mainFrame:GetCurrentMeter().title)
+    end)
 nextButton:SetPoint("LEFT", prevButton, "RIGHT", -3, 0)
 
 titleText:SetPoint("LEFT", nextButton, "RIGHT", 5, 0)
 titleText:SetText("Damage")
 
-local closeButton = CreateTitleBarButton("Interface\\Buttons\\UI-StopButton", function ()
-    commands:toggle()
-end)
+local closeButton = CreateTitleBarButton("Interface\\Buttons\\UI-StopButton",
+    function()
+        commands:toggle()
+    end)
 closeButton:SetPoint("RIGHT", frame, "RIGHT", -3, 0)
 
-local settingsButton = CreateTitleBarButton("Interface\\Buttons\\UI-OptionsButton", function ()
-    commands:config()
-end)
+local settingsButton = CreateTitleBarButton("Interface\\Buttons\\UI-OptionsButton",
+    function()
+        commands:config()
+    end)
 settingsButton:SetPoint("RIGHT", closeButton, "LEFT", -3, 0)
 
-local resetButton = CreateTitleBarButton("Interface\\Buttons\\UI-RefreshButton", function ()
-    commands:reset()
-end)
+local resetButton = CreateTitleBarButton("Interface\\Buttons\\UI-RefreshButton",
+    function()
+        commands:reset()
+    end)
 resetButton:SetPoint("RIGHT", settingsButton, "LEFT", -3, 0)
 
 nekometer.frames.titleBar = frame
